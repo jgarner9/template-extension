94a0f061b8575b8b3166530f3989abf886114dde
Jason <63689065+jgarner9@users.noreply.github.com>
2024-04-24 16:49:07 -0600

    basic add template modal

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+import EditorJS from "./editorjs.mjs";
+
 const templateOptionsElement = document.getElementById("template-options");
 const emptyTemplatesElement = document.getElementById("no-templates");
+const addTemplateButton = document.getElementById("add-template-button");
+const addTemplateModal = document.getElementById("add-template-modal");
+
+const addTemplate = () => {
+  addTemplateModal.showModal();
+};
+
+const editor = new EditorJS({
+  holder: "template-text-area",
+  placeholder: "Template text...",
+  toolbar: { image: { inlineToolbar: ["link"] } },
+});
 
 if (templateOptionsElement.children.length === 0) {
   emptyTemplatesElement.hidden = false;
 } else {
   emptyTemplatesElement.hidden = true;
 }
+
+addTemplateButton.addEventListener("click", () => addTemplate());
